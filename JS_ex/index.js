0e560d8072b4452cc2e8a5647f94e3e7e1145e96
seen2 <tiwarietc@gmail.com>
2022-12-26 17:20:17 +0530

    Some change and Fetch

--- JS_ex/index.js
+++ JS_ex/index.js
@@ -3,11 +3,11 @@ const os = require("os");
 
 console.log(greet());
 
-
 console.log("Dir Name>>>" + __dirname);
+console.log("dir:" + process.cwd());
 console.log("Dir Name>>>" + __filename);
 
-writeFileSync("hello.txt", "Hello World")
+writeFileSync("hello.txt", "Hello World");
 console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 //OS
 console.log(os.arch());
@@ -21,4 +21,12 @@ function greet() {
 
   return 1;
 }
+console.log("OS: " + os.type());
 
+//FETCH IN NODE
+fetch("https://next-ts-todo.vercel.app/api/users").then(res=>res.json()).then(result=>{
+  console.log(">>>>>>>>>>>>>>>>");
+  console.log(result);
+}).catch(err=>{
+  console.log(err);
+});
